95a6069182af6a82c0d4c32e2977ac71cb39339f
tetsup <tetsup.xperia@gmail.com>
2021-11-24 17:39:42 +0900

    adminユーザー追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_20_065916) do
+ActiveRecord::Schema.define(version: 2021_11_23_035913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "email", default: "", null: false
