53cb132d959d5f7eb3603b5a3895080e0e884fc4
tetsup <tetsup.xperia@gmail.com>
2021-12-04 00:37:50 +0900

    現在時刻より後のレッスンしか作成できないように

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,12 @@ class Lesson < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :start_time, uniqueness: { scope: :teacher_id }
   validates :language_id, presence: true
+  validate :start_time_expect_to_be_after_now
+
+  def start_time_expect_to_be_after_now
+    errors.add(:start_time, 'は現在より後の時刻を指定してください') if self.start_time < Time.now
+  end
+
   has_one :reservation, dependent: :destroy
   has_one :reserve_student, through: :reservation, source: :student
   belongs_to :teacher
