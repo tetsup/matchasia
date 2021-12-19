ccf52f4af224e5afa9f81adca88d9b1315778b63
tetsup <tetsup.xperia@gmail.com>
2021-12-19 23:22:29 +0900

    予約されていないレッスンに対するフィードバックのバリデーション

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,9 +1,14 @@
 class Feedback < ApplicationRecord
   validates :content, presence: true, length: { maximum: 200 }
   validate :lesson_is_finished
+  validate :lesson_is_not_reserved
 
   def lesson_is_finished
     errors.add(:lesson, '終了していないレッスンにはフィードバックできません') unless lesson.finished?
+  end
+
+  def lesson_is_not_reserved
+    errors.add(:lesson, '予約されていないレッスンにはフィードバックできません') if lesson.reservation.nil?
   end
 
   belongs_to :lesson
