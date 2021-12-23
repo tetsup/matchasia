b1a26575ac09fabf6e13a1fe10fa59a4b920b1c8
tetsup <tetsup.xperia@gmail.com>
2021-12-23 23:25:45 +0900

    デフォルトチケット数変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_19_143459) do
+ActiveRecord::Schema.define(version: 2021_12_23_141441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 2021_12_19_143459) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "tickets", default: 0, null: false
+    t.integer "tickets", default: 1, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_students_on_email", unique: true
