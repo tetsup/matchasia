6bf9996e797e6e824f3e0f2ca6f48e23330ecc26
tetsup <tetsup.xperia@gmail.com>
2022-02-09 12:11:28 +0900

    レッスン一覧取得のN+1修正

--- app/controllers/teachers/lessons_controller.rb
+++ app/controllers/teachers/lessons_controller.rb
@@ -2,7 +2,13 @@ class Teachers::LessonsController < ApplicationController
   before_action :authenticate_teacher!
 
   def index
-    @lessons = current_teacher.lessons.eager_load(:reserve_student).eager_load(:feedback).eager_load(:report).sorted
+    @lessons = current_teacher
+               .lessons
+               .eager_load(:reservation)
+               .eager_load(:reserve_student)
+               .eager_load(:feedback)
+               .eager_load(:report)
+               .sorted
   end
 
   def new
