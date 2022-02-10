4089a2534f87a5f7e761766a88f38dc3a58eff54
tetsup <tetsup.xperia@gmail.com>
2022-02-11 07:27:22 +0900

    言語別、講師別集計機能のspec

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -29,16 +29,16 @@ RSpec.describe Lesson, type: :model do
   describe 'count methods' do
     before do
       travel_to Time.new(2021, 12, 29)
-      teachers = FactoryBot.create_list(:teacher, 3, :continuous)
+      @teachers = FactoryBot.create_list(:teacher, 3, :continuous)
       students = FactoryBot.create_list(:student, 3, :continuous)
       lessons = [
-        FactoryBot.create(:lesson, teacher: teachers[0], start_time: Time.new(2021, 12, 30, 7), language_id: 1),
-        FactoryBot.create(:lesson, teacher: teachers[1], start_time: Time.new(2021, 12, 30, 7), language_id: 2),
-        FactoryBot.create(:lesson, teacher: teachers[2], start_time: Time.new(2021, 12, 30, 7), language_id: 3),
-        FactoryBot.create(:lesson, teacher: teachers[0], start_time: Time.new(2021, 12, 30, 13), language_id: 1),
-        FactoryBot.create(:lesson, teacher: teachers[1], start_time: Time.new(2021, 12, 30, 13), language_id: 2),
-        FactoryBot.create(:lesson, teacher: teachers[0], start_time: Time.new(2021, 12, 30, 22), language_id: 1),
-        FactoryBot.create(:lesson, teacher: teachers[2], start_time: Time.new(2022, 1, 1, 22), language_id: 3)
+        FactoryBot.create(:lesson, teacher: @teachers[0], start_time: Time.new(2021, 12, 30, 7), language_id: 1),
+        FactoryBot.create(:lesson, teacher: @teachers[1], start_time: Time.new(2021, 12, 30, 7), language_id: 2),
+        FactoryBot.create(:lesson, teacher: @teachers[2], start_time: Time.new(2021, 12, 30, 7), language_id: 3),
+        FactoryBot.create(:lesson, teacher: @teachers[1], start_time: Time.new(2021, 12, 30, 13), language_id: 1),
+        FactoryBot.create(:lesson, teacher: @teachers[2], start_time: Time.new(2021, 12, 30, 13), language_id: 2),
+        FactoryBot.create(:lesson, teacher: @teachers[2], start_time: Time.new(2021, 12, 30, 22), language_id: 1),
+        FactoryBot.create(:lesson, teacher: @teachers[0], start_time: Time.new(2022, 1, 1, 22), language_id: 3)
       ]
       reservations = [
         FactoryBot.create(:reservation, :continuous, lesson: lessons[0], student: students[0]),
@@ -78,6 +78,90 @@ RSpec.describe Lesson, type: :model do
       expect(count_data[[Date.new(2021, 12, 30), 7]]).to eq nil
       count_data = Lesson.reserved_count_by_date_and_time(Date.new(2022, 1, 2), Date.new(2022, 1, 5))
       expect(count_data[[Date.new(2022, 1, 1), 22]]).to eq nil
+    end
+
+    it 'counts lessons by language and month' do
+      count_data = Lesson.count_by_month_and_language
+      expect(count_data[[Date.new(2021, 12, 1) + 9.hours, 1]]).to eq 3
+      expect(count_data[[Date.new(2021, 12, 1) + 9.hours, 2]]).to eq 2
+      expect(count_data[[Date.new(2021, 12, 1) + 9.hours, 3]]).to eq 1
+      expect(count_data[[Date.new(2022, 1, 1) + 9.hours, 1]]).to eq nil
+      expect(count_data[[Date.new(2022, 1, 1) + 9.hours, 2]]).to eq nil
+      expect(count_data[[Date.new(2022, 1, 1) + 9.hours, 3]]).to eq 1
+    end
+
+    it 'counts reservations by language and month' do
+      count_data = Lesson.reserved_count_by_month_and_language
+      expect(count_data[[Date.new(2021, 12, 1) + 9.hours, 1]]).to eq 1
+      expect(count_data[[Date.new(2021, 12, 1) + 9.hours, 2]]).to eq 2
+      expect(count_data[[Date.new(2021, 12, 1) + 9.hours, 3]]).to eq nil
+      expect(count_data[[Date.new(2022, 1, 1) + 9.hours, 1]]).to eq nil
+      expect(count_data[[Date.new(2022, 1, 1) + 9.hours, 2]]).to eq nil
+      expect(count_data[[Date.new(2022, 1, 1) + 9.hours, 3]]).to eq 1
+    end
+
+    it 'counts lessons by teacher and month' do
+      count_data = Lesson.count_by_month_and_teacher
+      expect(count_data[[Date.new(2021, 12, 1) + 9.hours, @teachers[0].id]]).to eq 1
+      expect(count_data[[Date.new(2021, 12, 1) + 9.hours, @teachers[1].id]]).to eq 2
+      expect(count_data[[Date.new(2021, 12, 1) + 9.hours, @teachers[2].id]]).to eq 3
+      expect(count_data[[Date.new(2022, 1, 1) + 9.hours, @teachers[0].id]]).to eq 1
+      expect(count_data[[Date.new(2022, 1, 1) + 9.hours, @teachers[1].id]]).to eq nil
+      expect(count_data[[Date.new(2022, 1, 1) + 9.hours, @teachers[2].id]]).to eq nil
+    end
+
+    it 'counts reservations by teacher and month' do
+      count_data = Lesson.reserved_count_by_month_and_teacher
+      expect(count_data[[Date.new(2021, 12, 1) + 9.hours, @teachers[0].id]]).to eq 1
+      expect(count_data[[Date.new(2021, 12, 1) + 9.hours, @teachers[1].id]]).to eq 1
+      expect(count_data[[Date.new(2021, 12, 1) + 9.hours, @teachers[2].id]]).to eq 1
+      expect(count_data[[Date.new(2022, 1, 1) + 9.hours, @teachers[0].id]]).to eq 1
+      expect(count_data[[Date.new(2022, 1, 1) + 9.hours, @teachers[1].id]]).to eq nil
+      expect(count_data[[Date.new(2022, 1, 1) + 9.hours, @teachers[2].id]]).to eq nil
+    end
+
+    it 'counts lessons by language and date' do
+      count_data = Lesson.count_by_date_and_language(Date.new(2021, 12, 1), Date.new(2021, 12, 31))
+      expect(count_data[[Date.new(2021, 12, 30), 1]]).to eq 3
+      expect(count_data[[Date.new(2021, 12, 30), 2]]).to eq 2
+      expect(count_data[[Date.new(2021, 12, 30), 3]]).to eq 1
+      expect(count_data[[Date.new(2022, 1, 1), 3]]).to eq nil
+      count_data = Lesson.count_by_date_and_language(Date.new(2022, 1, 1), Date.new(2022, 1, 31))
+      expect(count_data[[Date.new(2022, 1, 1), 3]]).to eq 1
+      expect(count_data[[Date.new(2021, 12, 30), 1]]).to eq nil
+    end
+
+    it 'counts reservations by language and date' do
+      count_data = Lesson.reserved_count_by_date_and_language(Date.new(2021, 12, 1), Date.new(2021, 12, 31))
+      expect(count_data[[Date.new(2021, 12, 30), 1]]).to eq 1
+      expect(count_data[[Date.new(2021, 12, 30), 2]]).to eq 2
+      expect(count_data[[Date.new(2021, 12, 30), 3]]).to eq nil
+      expect(count_data[[Date.new(2022, 1, 1), 3]]).to eq nil
+      count_data = Lesson.reserved_count_by_date_and_language(Date.new(2022, 1, 1), Date.new(2022, 1, 31))
+      expect(count_data[[Date.new(2022, 1, 1), 3]]).to eq 1
+      expect(count_data[[Date.new(2021, 12, 30), 1]]).to eq nil
+    end
+
+    it 'counts lessons by teacher and date' do
+      count_data = Lesson.count_by_date_and_teacher(Date.new(2021, 12, 1), Date.new(2021, 12, 31))
+      expect(count_data[[Date.new(2021, 12, 30), @teachers[0].id]]).to eq 1
+      expect(count_data[[Date.new(2021, 12, 30), @teachers[1].id]]).to eq 2
+      expect(count_data[[Date.new(2021, 12, 30), @teachers[2].id]]).to eq 3
+      expect(count_data[[Date.new(2022, 1, 1), @teachers[0].id]]).to eq nil
+      count_data = Lesson.count_by_date_and_teacher(Date.new(2022, 1, 1), Date.new(2022, 1, 31))
+      expect(count_data[[Date.new(2022, 1, 1), @teachers[0].id]]).to eq 1
+      expect(count_data[[Date.new(2021, 12, 30), @teachers[0].id]]).to eq nil
+    end
+
+    it 'counts reservations by teacher and date' do
+      count_data = Lesson.reserved_count_by_date_and_teacher(Date.new(2021, 12, 1), Date.new(2021, 12, 31))
+      expect(count_data[[Date.new(2021, 12, 30), @teachers[0].id]]).to eq 1
+      expect(count_data[[Date.new(2021, 12, 30), @teachers[1].id]]).to eq 1
+      expect(count_data[[Date.new(2021, 12, 30), @teachers[2].id]]).to eq 1
+      expect(count_data[[Date.new(2022, 1, 1), @teachers[0].id]]).to eq nil
+      count_data = Lesson.reserved_count_by_date_and_teacher(Date.new(2022, 1, 1), Date.new(2022, 1, 31))
+      expect(count_data[[Date.new(2022, 1, 1), @teachers[0].id]]).to eq 1
+      expect(count_data[[Date.new(2021, 12, 30), @teachers[0].id]]).to eq nil
     end
   end
 end
