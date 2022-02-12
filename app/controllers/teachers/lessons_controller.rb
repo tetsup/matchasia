d42f47b0612a57db88b9d8e816da163d886f1549
tetsup <tetsup.xperia@gmail.com>
2022-02-13 08:50:50 +0900

    レッスン一括登録のエラー処理と、登録件数出力

--- app/controllers/teachers/lessons_controller.rb
+++ app/controllers/teachers/lessons_controller.rb
@@ -36,17 +36,21 @@ class Teachers::LessonsController < ApplicationController
   def bulk_create
     create_params = bulk_create_params
     ActiveRecord::Base.transaction do
+      create_count = 0
       create_params[:start_time].each do |start_time, flag|
         next if flag != '1'
 
-        next if current_teacher.lessons.create(
+        lesson = current_teacher.lessons.create(
           language_id: create_params[:language_id],
           start_time: Time.strptime(start_time, '%Y-%m-%d %H')
         )
-
-        render :bulk_new
+        if lesson.save
+          create_count += 1
+        else
+          flash[:alert] = "#{lesson.start_time.to_s(:datetime_jp)}のレッスン登録に失敗しました(#{lesson.errors.full_messages})"
+        end
       end
-      redirect_to teachers_lessons_path, notice: 'レッスンを一括登録しました'
+      redirect_to teachers_lessons_path, notice: "#{create_count}件のレッスンを一括登録しました"
     end
   end
 
