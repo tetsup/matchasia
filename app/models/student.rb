a1b0c8add7ad55fb7f26479564e65b6824f76fd2
tetsup <tetsup.xperia@gmail.com>
2022-02-24 21:12:00 +0900

    with_optionsで統合

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,17 +4,18 @@ class Student < ApplicationRecord
   has_many :reservations
   has_many :payments
 
-  validates :username,
-    presence: true,
-    length: { in: 3..20 },
-    format: { with: /\A[a-zA-Z][a-zA-Z0-9]+\z/ },
-    uniqueness: true
-  validates :tickets,
-    presence: true,
-    numericality: {
-      only_integer: true,
-      greater_than_or_equal_to: 0
-    }
+  with_options presence: true do
+    validates :username,
+      length: { in: 3..20 },
+      format: { with: /\A[a-zA-Z][a-zA-Z0-9]+\z/ },
+      uniqueness: true
+    validates :tickets,
+      numericality: {
+        only_integer: true,
+        greater_than_or_equal_to: 0
+      }
+  end
+
   def create_stripe_customer_id
     return if stripe_customer_id.present?
 
